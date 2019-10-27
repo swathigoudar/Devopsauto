9fd38de1bcaa487036b1c2935a07281d5264c9a8
swathigoudar <swathigoudar@gmail.com>
2019-10-27 11:39:22 +0530

    Add files via upload
    first commit

---
+++ ansible.sh
@@ -0,0 +1,6 @@
+
+cd home/ubuntu
+rm -rf ansibleRepo
+git clone https://github.com/swathigoudar/ansibleRepo.git
+cd ansibleRepo
+ansible-playbook -v ansible.yml
---
+++ app.js
@@ -0,0 +1,67 @@
+
+//Express Params
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
+app.set('port', process.env.PORT || 7878);
+
+var cmd=require('node-cmd');
+
+app.post('/gitPush', function (req, res) {
+console.log("Received the message inside gitPush->"+ JSON.stringify(req.body));
+
+    cmd.get('bash script.sh',
+        function(err, data, stderr){
+            if (!err) {
+               console.log('Error in Running the Code->',data)
+            } else {
+               console.log('error', err)
+            }
+
+        }
+    );
+
+
+});
+app.post('/ansible', function (req, res) {
+    console.log("Received the message inside gitPush->"+ JSON.stringify(req.body));
+    
+        cmd.get('bash ansible.sh',
+            function(err, data, stderr){
+                if (!err) {
+                   console.log('Error in Running the Code->',data)
+                } else {
+                   console.log('error', err)
+                }
+    
+            }
+        );
+    
+    
+    });
+
+    app.get('/ansible', function (req, res) {
+        console.log("Received the message inside gitPush->"+ JSON.stringify(req.body));
+        
+            cmd.get('bash ansible.sh',
+                function(err, data, stderr){
+                    if (!err) {
+                       console.log('Error in Running the Code->',data)
+                    } else {
+                       console.log('error', err)
+                    }
+        
+                }
+            );
+        
+        
+        });
+
+
+
+
+app.listen(app.get('port'));
